05c77c675b106bd6f85a1192a6af084c25708802
banaanihillo <banaanihillo@protonmail.ch>
2020-04-12 15:30:19 +0300

    Commited exercise4-5 (Blog jest)
    Added testing of favorite blog

--- utils/tester.js
+++ utils/tester.js
@@ -2,12 +2,22 @@ const timesThanked = (blogs) => {
     const arrayOfThanks = blogs.map(blog => {
         return blog.thanks
     })
-    console.log(arrayOfThanks)
     
     const total = (sum, individualThanks) => {
         return sum + individualThanks
     }
     return arrayOfThanks.reduce(total, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    let highestThusFar = 0
+    blogs.map(blog => {
+        if (blog.thanks > highestThusFar) {
+            highestThusFar = blog.thanks
+            return highestThusFar
+        }
+    })
+    return blogs.find(favorite => favorite.thanks === highestThusFar)
 }
 
 const dummy = () => {
@@ -16,5 +26,6 @@ const dummy = () => {
 
 module.exports = {
     timesThanked,
+    favoriteBlog,
     dummy
 }
