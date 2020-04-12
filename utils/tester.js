e06e0504caed1923c3f8b94d92275127360cf262
banaanihillo <banaanihillo@protonmail.ch>
2020-04-12 13:10:15 +0300

    Commited exercise 4-4 (Blog jest)
    Added tests for counting the times certain blogs have been thanked

--- utils/tester.js
+++ utils/tester.js
@@ -1,5 +1,20 @@
-const dummy = (blogs) => {
+const timesThanked = (blogs) => {
+    const arrayOfThanks = blogs.map(blog => {
+        return blog.thanks
+    })
+    console.log(arrayOfThanks)
+    
+    const total = (sum, individualThanks) => {
+        return sum + individualThanks
+    }
+    return arrayOfThanks.reduce(total, 0)
+}
+
+const dummy = () => {
     return 1
 }
 
-module.exports = {dummy}
+module.exports = {
+    timesThanked,
+    dummy
+}
